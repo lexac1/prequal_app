7b79bedb2c674de49472c29b4f166b375e70e07e
Alexis Carroll <lex.ac1@gmail.com>
2017-01-31 19:33:36 -0800

    Initial commit with outline

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  getPrequalData();
+})
+
+var getPrequalData = function () {
+  $('form').submit(function () {
+    event.preventDefault();
+    var data = $('form').serializeArray()
+    var mortagageTypes = data[0].value
+    var salesPrice = data[1].value
+    var downPayment = data[2].name
+    var income = data[3].value
+    var liabilities = data[4].value
+
+    if (mortagageType(mortagageTypes)){
+      console.log('fha')
+    }
+    debtToIncomeRatio(income, liabilities)
+  })
+}
+
+var mortagageType = function (mortagageTypes) {
+  if (mortagageTypes === 'fha') {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
+}
